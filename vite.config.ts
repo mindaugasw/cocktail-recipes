c3a01d64372069d982590f278d94a152b7a4d170
Mindaugas Vaitiekūnas <mindaugas932@gmail.com>
2025-09-28 15:46:06 +0300

    Update BASE_PATH for subdomain use
    Update BASE_PATH from '/cocktail-recipes' (used for GitHub Pages domain, under '/cocktail-recipes' path) to '', needed for hosting under custom subdomain on root path '/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-const BASE_PATH = '/cocktail-recipes';
+// This can be used to put entire app under a speicfic path.
+// E.g. for GitHub Pages hosting, without a custom domain,
+// this was '/cocktail-recipes' to work under https://mindaugasw.github.io/cocktail-recipes/
+// Then 'base' should also be uncommented under defineConfig() below.
+//
+// For hosting under a custom subdomain, this needs to be empty string ''
+// ('/' is not suitable because it generates img URLs like '//images/img.png'. But could be fixed in code if needed)
+const BASE_PATH = '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,5 +33,5 @@ export default defineConfig({
         __BUILD_VERSION__: Date.now().toString(),
         __BASE_PATH__: `'${BASE_PATH}'`,
     },
-    base: `${BASE_PATH}/`,
+    // base: BASE_PATH,
 })
